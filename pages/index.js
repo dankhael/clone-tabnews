717929628c585000f5313c634d3faa3841fe7e04
Danilo Mikhael da Silva Melo <danilokhael@gmail.com>
2025-12-24 08:31:15 +0000

    Adicionando mensagem de natal

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ function Home() {
     return (
     <div>
         <img src={lainGif.src} alt="Computer girl" style={{ width: 480, height: 480 }}/>
+        <h1>Feliz Natal Noobs 🎄</h1>
     </div>
 )
           
